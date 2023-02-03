5e4868548dd81627441d2f508c23f567f1f8f0b3
saad <skyeayyad@gmail.com>
2023-02-03 16:28:53 +0100

    loading component styled

--- helpers/Spinner.js
+++ helpers/Spinner.js
@@ -1,12 +1,19 @@
 import React from "react";
 import classes from "./Spinner.module.scss";
+import Image from "next/image";
 const Spinner = () => {
   return (
     <div className={classes.center}>
-    <div className={classes.lds_ripple}>
-      <div></div>
-      <div></div>
-    </div>
+      <div className={classes.lds_ripple}>
+        <div></div>
+        <div></div>
+      </div>
+      <Image
+        src={"/images/NewYorkTimes.svg"}
+        alt="newyorktimes logo"
+        width={400}
+        height={57.14}
+      />
     </div>
   );
 };
