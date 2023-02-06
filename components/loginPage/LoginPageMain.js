9cf2701b0ca644cd71cba481001d2732c7cb96e4
saad <skyeayyad@gmail.com>
2023-02-06 20:49:22 +0100

    login page

--- components/loginPage/LoginPageMain.js
+++ components/loginPage/LoginPageMain.js
@@ -5,8 +5,10 @@ const LoginPageMain = () => {
     <>
       <h2 className={classes.pageHeading}>Log in or create an account</h2>
       <form className={classes.loginForm}>
-        <label for="email">Email Address</label>
-        <input type="email" name="email" />
+        <div className={classes.FormInput}>
+          <label for="email">Email Address</label>
+          <input type="email" name="email" />
+        </div>
         <button>Continue</button>
       </form>
     </>
