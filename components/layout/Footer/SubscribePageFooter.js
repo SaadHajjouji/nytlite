58dfeeadbc3860fca3dad7d09de4a311aad04cde
saad <skyeayyad@gmail.com>
2023-02-12 16:30:34 +0100

    login page done

---
+++ components/layout/Footer/SubscribePageFooter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./SubscribePageFooter.module.scss";
+const SubscribePageFooter = () => {
+  return (
+    <footer className={classes.Footer}>
+      This project is for learning purposes only. All rights go to The new york
+      times company.
+    </footer>
+  );
+};
+
+export default SubscribePageFooter;
